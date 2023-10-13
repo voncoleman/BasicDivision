8bd6401a45526e3e4c3ca445bb8d53c4a0caeb80
Von Coleman <von.coleman@yahoo.com>
2023-10-12 21:17:02 -0500

    first commit

---
+++ src/BasicDivision.java
@@ -0,0 +1,5 @@
+public class BasicDivision { //The name of the class is BasicDivision. The class is public. We're going to do this ad nauseam and you're gonna love it!
+    public static void main(String[] args){ //The beginning of the BasicDivision program. 
+        System.out.println(100/10); //System.out.println is gonna display the result of the two numbers being divided. Division Operator '/'
+    }
+}
